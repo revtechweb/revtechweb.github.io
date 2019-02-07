9d97f897d96638eb6f18a55c7e28560e81e5983d
Matheus <lima.eca@gmail.com>
2019-02-07 18:52:57 -0200

    react app

---
+++ precache-manifest.ea9cefb9efb610d66fd5aa568d9af8de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "70e5c9f557ef3ff1246e678e898869f5",
+    "url": "./static/media/aktienow.70e5c9f5.png"
+  },
+  {
+    "revision": "91c58e762582a3a64a38",
+    "url": "./static/css/main.dd1d5cd2.chunk.css"
+  },
+  {
+    "revision": "cf7af9b081c00fc193df",
+    "url": "./static/js/1.cf7af9b0.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "65cb22c0511f433245bdd257357704f9",
+    "url": "./static/media/logo.65cb22c0.png"
+  },
+  {
+    "revision": "49d7a1ecf5af7dd6acb311d747a491fb",
+    "url": "./static/media/background.49d7a1ec.png"
+  },
+  {
+    "revision": "91c58e762582a3a64a38",
+    "url": "./static/js/main.91c58e76.chunk.js"
+  },
+  {
+    "revision": "4c8aae6ca730b583eb0417419dbbaf06",
+    "url": "./static/media/hexagon.4c8aae6c.png"
+  },
+  {
+    "revision": "9570a011f0b18ee63d712765d9461066",
+    "url": "./static/media/vivo.9570a011.svg"
+  },
+  {
+    "revision": "1705ad827793160c643359bda8ccee7f",
+    "url": "./static/media/hypnobox.1705ad82.png"
+  },
+  {
+    "revision": "caef48f8375bc041f4255eca7f03b87e",
+    "url": "./static/media/logo_revtechweb_horizontal_branco.caef48f8.png"
+  },
+  {
+    "revision": "fd21133ca49bfe8627951f7372e9dc02",
+    "url": "./static/media/logo_revtechweb_vertical_branco.fd21133c.png"
+  },
+  {
+    "revision": "4d92d562cf967abd993be8e2501239c8",
+    "url": "./index.html"
+  }
+];
